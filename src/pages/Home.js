fd7fb8f8f6bfb3aaf31a88ba0a74a53e2bfddb26
Flo-le-dev <f.gironde.dev@gmail.com>
2024-04-23 23:55:25 +0200

    ended p 11 javascript

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,29 @@ import "../styles/style.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Card from "../components/Card";
+import { useEffect, useState } from "react";
 
 const Home = () => {
-  let data = require("../data/data.json");
-  console.log(data);
+  const [data, setData] = useState(null);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/data/data.json");
+        if (!response.ok) {
+          throw new Error("Network response was not ok.");
+        }
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(data);
   return (
     <div>
       <Navigation />
@@ -16,9 +34,10 @@ const Home = () => {
         <Header children={<h1>Chez vous, partout et ailleurs</h1>} />
         <div className="logementCardContainer">
           <ul>
-            {data.map((logement) => {
-              return <Card key={logement.id} logement={logement} />;
-            })}
+            {data &&
+              data.map((logement) => {
+                return <Card key={logement.id} logement={logement} />;
+              })}
           </ul>
         </div>
       </main>
--- src/pages/Logement.js
+++ src/pages/Logement.js
@@ -4,28 +4,59 @@ import Navigation from "../components/Navigation";
 import Carousel from "../components/Carousel.js";
 import Information from "../components/Information";
 import Footer from "../components/Footer";
+import { useEffect, useState } from "react";
 
 const Logement = () => {
-  // DATA = objet gloable (20)
-  //   !FETCH
-  let data = require("../data/data.json");
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/data/data.json");
+        if (!response.ok) {
+          throw new Error("Network response was not ok.");
+        }
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log("DATA", data);
 
   //   PARAM = id de la âge voulue
   const { id } = useParams();
   console.log(id, "====PARAM");
 
   //   filteredData = les informations complètes de PARAM
-  const filteredData = data.filter((obj) => {
-    return obj.id === id;
-  })[0];
+  let filteredData;
+
+  if (data) {
+    filteredData = data.filter((obj) => {
+      return obj.id === id;
+    })[0];
+  }
 
   console.log("===filteredData===", filteredData);
 
   return (
     <div>
       <Navigation />
-      <Carousel filteredData={filteredData} />
-      <Information filteredData={filteredData} />
+      {!data ? (
+        <p>Aucun résultat ne correspond à votre recherche.</p>
+      ) : filteredData ? (
+        <>
+          <Carousel filteredData={filteredData} />
+          <Information filteredData={filteredData} />
+        </>
+      ) : (
+        <p>Aucun logement trouvé pour l'ID : {id}</p>
+      )}
+
       <Footer />
     </div>
   );
